9c6f00447ef44557fbf8abec28f998d6e52a4399
f3liip3182 <feliignacio.fpv@gmail.com>
2025-06-28 17:43:27 -0400

    ajustes valoracion

--- backend-alertaya/src/routes/valoraciones.js
+++ backend-alertaya/src/routes/valoraciones.js
@@ -6,5 +6,6 @@ const verifyToken = require('../middlewares/auth.middleware');
 router.post('/:id', verifyToken, valorarReporte);
 router.get('/usuario/:reporteId', verifyToken, obtenerValoracionUsuario);
 router.get('/:id', obtenerResumenValoraciones);
+router.delete('/:id', verifyToken, eliminarValoracion);
 
 module.exports = router;

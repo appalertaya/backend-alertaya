4d83222a9e04fc0d296d2a1f0a78b74bca9a68da
f3liip3182 <feliignacio.fpv@gmail.com>
2025-06-28 16:39:20 -0400

    cambio a ruta de render

--- backend-alertaya/src/config/db.js
+++ backend-alertaya/src/config/db.js
@@ -3,21 +3,26 @@ const mysql = require('mysql2');
 
 console.log('📦 DATABASE_URL:', process.env.DATABASE_URL);
 
-const pool = mysql.createPool(process.env.DATABASE_URL, {
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let pool;
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err.message);
-  } else {
-    console.log('🟢 Conexión a MySQL establecida correctamente');
-    connection.release();
-  }
-});
+function createPoolWithRetry() {
+  pool = mysql.createPool(process.env.DATABASE_URL, {
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('❌ Error al conectar a la base de datos:', err.message);
+      setTimeout(createPoolWithRetry, 5000); // Reintenta luego de 5 segundos
+    } else {
+      console.log('🟢 Conexión a MySQL establecida correctamente');
+      connection.release();
+    }
+  });
+}
 
+createPoolWithRetry();
 
 module.exports = pool;

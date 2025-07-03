4e97c40fd74cbe59632c11e3a92d46acf237b59f
f3liip3182 <feliignacio.fpv@gmail.com>
2025-07-03 01:20:46 -0400

    enviar notificaciones push antes de prueba 5

--- backend-alertaya/src/utils/sendNotification.js
+++ backend-alertaya/src/utils/sendNotification.js
@@ -7,36 +7,37 @@ const haversine = require('haversine-distance');
  * @param {Array} usuarios - Usuarios con tokenFCM y ubicación registrada.
  */
 async function enviarNotificacionesUsuariosCercanos(datosReporte, usuarios) {
-  const { lat, lng, ciudad, categoria } = datosReporte;
-  const puntoReporte = { latitude: parseFloat(lat), longitude: parseFloat(lng) };
+    console.log("datosReporte: ",datosReporte)
+    const { lat, lng, ciudad, categoria } = datosReporte;
+    const puntoReporte = { latitude: parseFloat(lat), longitude: parseFloat(lng) };
 
-  const usuariosCercanos = usuarios.filter(usuario => {
-    const puntoUsuario = {
-      latitude: parseFloat(usuario.ultimaLat),
-      longitude: parseFloat(usuario.ultimaLng),
-    };
-    const distancia = haversine(puntoReporte, puntoUsuario); // metros
-    return distancia <= 10000; // máximo 10 km
-  });
+    const usuariosCercanos = usuarios.filter(usuario => {
+        const puntoUsuario = {
+            latitude: parseFloat(usuario.ultimaLat),
+            longitude: parseFloat(usuario.ultimaLng),
+        };
+        const distancia = haversine(puntoReporte, puntoUsuario); // metros
+        return distancia <= 10000; // máximo 10 km
+    });
 
-  console.log(`Usuarios dentro de 10 km: ${usuariosCercanos.length}`);
+    console.log(`Usuarios dentro de 10 km: ${usuariosCercanos.length}`);
 
-  for (const usuario of usuariosCercanos) {
-    const message = {
-      notification: {
-        title: `Nuevo reporte cercano: ${categoria}`,
-        body: `Se registró un incidente en tu zona (${ciudad}).`,
-      },
-      token: usuario.tokenFCM,
-    };
+    for (const usuario of usuariosCercanos) {
+        const message = {
+            notification: {
+                title: `Nuevo reporte cercano: ${categoria}`,
+                body: `Se registró un incidente en tu zona (${ciudad}).`,
+            },
+            token: usuario.tokenFCM,
+        };
 
-    try {
-      await admin.messaging().send(message);
-      console.log(`✅ Notificación enviada a: ${usuario.tokenFCM}`);
-    } catch (err) {
-      console.warn(`⚠️ Error al enviar notificación: ${err.message}`);
+        try {
+            await admin.messaging().send(message);
+            console.log(`✅ Notificación enviada a: ${usuario.tokenFCM}`);
+        } catch (err) {
+            console.warn(`⚠️ Error al enviar notificación: ${err.message}`);
+        }
     }
-  }
 }
 
 module.exports = enviarNotificacionesUsuariosCercanos;
